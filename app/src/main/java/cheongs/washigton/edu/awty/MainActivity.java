dba3c613793a2f38dc4c86b2063215b34bdbca3f
Sean H. Cheong <cheongs@ougl-34-d.eplt.washington.edu>
2015-05-14 15:23:53 -0700

    screenshots

--- app/src/main/java/cheongs/washigton/edu/awty/MainActivity.java
+++ app/src/main/java/cheongs/washigton/edu/awty/MainActivity.java
@@ -1,18 +1,23 @@
 package cheongs.washigton.edu.awty;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private PendingIntent alarmIntent;
+    private AlarmManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,29 @@ public class MainActivity extends ActionBarActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int phoneNumber = Integer.parseInt(phone.getText().toString());
-                int delayValue = Integer.parseInt(delay.getText().toString());
+                // For the delay, only numbers can be inputted because I set the Input Type to be numbers.
+                // The other two just needs to have values or than an empty string
                 if(!message.getText().toString().equals("") && (!phone.getText().equals("")) && !delay.getText().equals("")){
-                    start.setText("Stop");
-                    Intent intent = new Intent(MainActivity.this, Receiver.class );
+                    if(start.getText().equals("START")) {
+                        start.setText("STOP");
+                        Intent intent = new Intent(MainActivity.this, Receiver.class);
+                        intent.putExtra("passedMessage", message.getText().toString());
+                        intent.putExtra("passedPhone", phone.getText().toString());
+                        alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+                        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                        int delayTimer = Integer.parseInt(delay.getText().toString()) * 60000; // 60,000 ms per minute
+                        am.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), delayTimer, alarmIntent);
+
+                        // Toasts have delay once "START" has been pressed. This toast is for right when "START" is clicked
+                        Toast.makeText(MainActivity.this, phone.getText() + ": " + message.getText(), Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        start.setText("START");
+                        am.cancel(alarmIntent);
+                        alarmIntent.cancel();
+                    }
                 }
             }
         });
--- app/src/main/java/cheongs/washigton/edu/awty/Receiver.java
+++ app/src/main/java/cheongs/washigton/edu/awty/Receiver.java
@@ -3,6 +3,7 @@ package cheongs.washigton.edu.awty;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * Created by cheongs on 5/14/15.
@@ -11,6 +12,9 @@ public class Receiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        String message = intent.getStringExtra("passedMessage");
+        String phoneNumber = intent.getStringExtra("passedPhone");
+        Toast.makeText(context, phoneNumber + ": " + message, Toast.LENGTH_SHORT).show();
     }
 }
+
